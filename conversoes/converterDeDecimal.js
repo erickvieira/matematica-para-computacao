b150f2766e15f54b54773d4f8f5dabb2d1a8bdcc
Erick Vieira <erick-vieira-s@outlook.com>
2020-07-08 20:25:36 -0300

    incluindo suporte à hex string

--- conversoes/converterDeDecimal.js
+++ conversoes/converterDeDecimal.js
@@ -1,4 +1,8 @@
 function converterDeDecimal(decimal = 0, base = 2) {
+  if (![2, 8, 16].includes(base)) {
+    throw new Error("Please, use one of these bases: 2, 8 or 16");
+  }
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
   if (decimal > 0) {
     if (decimal < base) {
       return decimal;
@@ -12,7 +16,9 @@ function converterDeDecimal(decimal = 0, base = 2) {
       quociente = Math.floor(quociente / base);
     }
 
-    return +buffer.join("");
+    return base === 16
+      ? buffer.map((algarismo) => hex[algarismo]).join("")
+      : +buffer.join("");
   } else {
     return 0;
   }
--- conversoes/converterParaDecimal.js
+++ conversoes/converterParaDecimal.js
@@ -1,13 +1,35 @@
 function converterParaDecimal(numero = 0, base = 2) {
-  if (numero > 0) {
-    const algarismos = `${numero}`.split("");
+  if (![2, 8, 16].includes(base)) {
+    throw new Error("Please, use one of these bases: 2, 8 or 16");
+  }
+  const hex = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    A: 10,
+    B: 11,
+    C: 12,
+    D: 13,
+    E: 14,
+    F: 15,
+  };
+  if (numero > 0 || (base === 16 && numero.length > 0)) {
+    const algarismos = `${numero}`.split("").map((alg) => hex[alg]);
     const pesos = algarismos.map((algarismo, i, { length }) => ({
       algarismo: +algarismo,
-      peso: length - i - 1
+      peso: length - i - 1,
     }));
-    return pesos.reduce((acc, { algarismo, peso }) => (
-      acc + (algarismo * Math.pow(base, peso))
-    ), 0)
+    return pesos.reduce(
+      (acc, { algarismo, peso }) => acc + algarismo * Math.pow(base, peso),
+      0
+    );
   } else {
     return 0;
   }
